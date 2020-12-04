381cd16db62e49cf4031c5b1759cb74f1390a420
Robin LECORVAISIER <47104799+robinlecorvaisier@users.noreply.github.com>
2020-12-04 06:01:05 +0100

    icone connection

--- public/index.php
+++ public/index.php
@@ -1,3 +1,19 @@
 <?php
-header("Location: ./router.php/?controller=home_controller");
-exit();
+
+$controller = (!empty($_GET["controller"])) ? $_GET["controller"] : 'home_controller'   ;
+
+$route = '../src/Controller/';
+
+$valid_controller = [
+    "test_controller",
+    "home_controller",
+];
+
+// test validation page
+foreach($valid_controller as $valid){
+    if ($valid == $controller){
+        require($route . $controller.'.php');
+    } else {
+        require($route.'404_controller.php');
+    }
+}
