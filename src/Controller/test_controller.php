683a8afa2ee39297f327ee28a30d527c12f7b7c1
AnthoSG <idarkmaster@laposte.net>
2020-12-04 03:42:14 +0100

    layout_test

--- src/Controller/test_controller.php
+++ src/Controller/test_controller.php
@@ -2,8 +2,12 @@
 // Ici interaction avec le model 
 // instanciation des variables à passer à la vue
 
-
 $name = "robin";
+$body = "test";
+
+require('./templates/views/layout_test.phtml');
+/*
 require('./templates/components/header.phtml');
 require('./templates/views/test.phtml');
 require('./templates/components/footer.phtml');
+*/
